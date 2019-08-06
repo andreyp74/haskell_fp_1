7e6fd9d07af8cf08990220f20c5add716cf8bcd1
Andrei Petrov <petrov.andrey@gmail.com>
2019-08-06 19:33:09 +0200

    continue 4.6

--- part4_types/4.6/maybe_monoid.hs
+++ part4_types/4.6/maybe_monoid.hs
@@ -9,19 +9,17 @@ newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
     deriving (Eq,Show)
 
 instance Semigroup a => Semigroup (Maybe' a) where
-    Maybe' (Just x) <> Maybe' (Just y) = Maybe' $ Just ( x <> y)
-    Maybe' (Just x) <> Maybe' Nothing  = Maybe' (Just x) 
-    Maybe' Nothing  <> other = other
+    Maybe' (Just x) <> Maybe' (Just y) = Maybe' $ Just (x <> y)
+    Maybe' Nothing <> _ = Maybe' Nothing
+    _ <> Maybe' Nothing = Maybe' Nothing
 
 instance Monoid a => Monoid (Maybe' a) where
     mempty = Maybe' (Just mempty)
    
-
-
 -- instance Monoid a => Monoid (Maybe' a) where
 --     mempty = Maybe' (Just mempty)
---     Maybe' (Just x) `mappend` Maybe' (Just y) = Maybe' $ Just ( x `mappend` y)
---     Maybe' (Just x) `mappend` Maybe' Nothing  = Maybe' (Just x) 
---     Maybe' Nothing  `mappend` other = other
+--     Maybe' (Just x) `mappend` Maybe' (Just y) = Maybe' $ Just (x `mappend` y)
+--     Maybe' Nothing `mappend` _ = Maybe' Nothing
+--     _ `mappend` Maybe' Nothing = Maybe' Nothing
 
 test0 = if (mempty :: Maybe' [Int]) == Maybe' Nothing then "failed" else "passed"
